b227c6373c7215df5ccdbd641db51143c6ebd297
Ryan Mason <ryanmasonmusic@gmail.com>
2022-11-28 21:49:20 -0500

    commit tree f70e8be519ebfc719e4d3f355a9d315e4998eb39

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendCat(name){
+    return cats.push(name);
+}
+function destructivelyPrependCat(name){
+    return cats.unshift(name);
+}
+function destructivelyRemoveLastCat(){
+    return cats.pop();
+}
+function destructivelyRemoveFirstCat(){
+    return cats.shift();
+}
+function appendCat(name){
+    const copyOfCats = cats.slice();
+    copyOfCats.push(name);
+    return copyOfCats;
+}
+function prependCat(name){
+    const copyOfCats = cats.slice();
+    copyOfCats.unshift(name);
+    return copyOfCats;
+}
+function removeLastCat(){
+    const copyOfCats = cats.slice();
+    copyOfCats.pop();
+    return copyOfCats;
+}
+function removeFirstCat(){
+    const copyOfCats = cats.slice();
+    copyOfCats.shift();
+    return copyOfCats;
+}
